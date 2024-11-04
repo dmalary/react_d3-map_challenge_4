5f7b430629a83f5f8e2a9cc5e7acbc8b31518f93
madhat5 <davidmalary@gmail.com>
2024-11-04 15:01:11 -0500

    feat: add data

---
+++ data/numData.ts
@@ -0,0 +1,257 @@
+export const numData: { code: string; value: number; name: string }[] = [
+  {
+    code: 'Alabama',
+    name: 'Alabama',
+    value: 0,
+  },
+  {
+    code: 'Alaska',
+    name: 'Alaska',
+    value: 0,
+  },
+  {
+    code: 'Arizona',
+    name: 'Arizona',
+    value: 3,
+  },
+  {
+    code: 'Arkansas',
+    name: 'Arkansas',
+    value: 0,
+  },
+  {
+    code: 'California',
+    name: 'California',
+    value: 15,
+  },
+  {
+    code: 'Colorado',
+    name: 'Colorado',
+    value: 4,
+  },
+  {
+    code: 'Connecticut',
+    name: 'Connecticut',
+    value: 0,
+  },
+  {
+    code: 'Delaware',
+    name: 'Delaware',
+    value: 0,
+  },
+  {
+    code: 'District of Columbia',
+    name: 'District of Columbia',
+    value: 3,
+  },
+  {
+    code: 'Florida',
+    name: 'Florida',
+    value: 9,
+  },
+  {
+    code: 'Georgia',
+    name: 'Georgia',
+    value: 3,
+  },
+  {
+    code: 'Hawaii',
+    name: 'Hawaii',
+    value: 0,
+  },
+  {
+    code: 'Idaho',
+    name: 'Idaho',
+    value: 0,
+  },
+  {
+    code: 'Illinois',
+    name: 'Illinois',
+    value: 5,
+  },
+  {
+    code: 'Indiana',
+    name: 'Indiana',
+    value: 2,
+  },
+  {
+    code: 'Iowa',
+    name: 'Iowa',
+    value: 0,
+  },
+  {
+    code: 'Kansas',
+    name: 'Kansas',
+    value: 0,
+  },
+  {
+    code: 'Kentucky',
+    name: 'Kentucky',
+    value: 0,
+  },
+  {
+    code: 'Louisiana',
+    name: 'Louisiana',
+    value: 2,
+  },
+  {
+    code: 'Maine',
+    name: 'Maine',
+    value: 0,
+  },
+  {
+    code: 'Maryland',
+    name: 'Maryland',
+    value: 3,
+  },
+  {
+    code: 'Massachusetts',
+    name: 'Massachusetts',
+    value: 4,
+  },
+  {
+    code: 'Michigan',
+    name: 'Michigan',
+    value: 4,
+  },
+  {
+    code: 'Minnesota',
+    name: 'Minnesota',
+    value: 4,
+  },
+  {
+    code: 'Mississippi',
+    name: 'Mississippi',
+    value: 0,
+  },
+  {
+    code: 'Missouri',
+    name: 'Missouri',
+    value: 4,
+  },
+  {
+    code: 'Montana',
+    name: 'Montana',
+    value: 0,
+  },
+  {
+    code: 'Nebraska',
+    name: 'Nebraska',
+    value: 0,
+  },
+  {
+    code: 'Nevada',
+    name: 'Nevada',
+    value: 2,
+  },
+  {
+    code: 'New Hampshire',
+    name: 'New Hampshire',
+    value: 0,
+  },
+  {
+    code: 'New Jersey',
+    name: 'New Jersey',
+    value: 3,
+  },
+  {
+    code: 'New Mexico',
+    name: 'New Mexico',
+    value: 0,
+  },
+  {
+    code: 'New York',
+    name: 'New York',
+    value: 8,
+  },
+  {
+    code: 'North Carolina',
+    name: 'North Carolina',
+    value: 3,
+  },
+  {
+    code: 'North Dakota',
+    name: 'North Dakota',
+    value: 0,
+  },
+  {
+    code: 'Ohio',
+    name: 'Ohio',
+    value: 6,
+  },
+  {
+    code: 'Oklahoma',
+    name: 'Oklahoma',
+    value: 1,
+  },
+  {
+    code: 'Oregon',
+    name: 'Oregon',
+    value: 1,
+  },
+  {
+    code: 'Pennsylvania',
+    name: 'Pennsylvania',
+    value: 7,
+  },
+  {
+    code: 'Rhode Island',
+    name: 'Rhode Island',
+    value: 0,
+  },
+  {
+    code: 'South Carolina',
+    name: 'South Carolina',
+    value: 0,
+  },
+  {
+    code: 'South Dakota',
+    name: 'South Dakota',
+    value: 0,
+  },
+  {
+    code: 'Tennessee',
+    name: 'Tennessee',
+    value: 3,
+  },
+  {
+    code: 'Texas',
+    name: 'Texas',
+    value: 8,
+  },
+  {
+    code: 'Utah',
+    name: 'Utah',
+    value: 2,
+  },
+  {
+    code: 'Vermont',
+    name: 'Vermont',
+    value: 0,
+  },
+  {
+    code: 'Virginia',
+    name: 'Virginia',
+    value: 0,
+  },
+  {
+    code: 'Washington',
+    name: 'Washington',
+    value: 3,
+  },
+  {
+    code: 'West Virginia',
+    name: 'West Virginia',
+    value: 0,
+  },
+  {
+    code: 'Wisconsin',
+    name: 'Wisconsin',
+    value: 3,
+  },
+  {
+    code: 'Wyoming',
+    name: 'Wyoming',
+    value: 0,
+  },
+];
